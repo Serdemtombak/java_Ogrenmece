64df738366daf78d602cac779d8e6ed6ff9e55b9
Serdemtombak <serdem.tombak@gmail.com>
2023-12-06 21:15:45 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world"); //line
+        System.out.println("hello");
+        System.out.println("Merhaba ben mustafa" +
+                 " bugun sizlerle" +
+                 " java ogreniyoruz");
+        System.out.println(10 + "15");
+        System.out.println(10 + 15);
+        System.out.print("merhaba\n dunya\n");
+        System.out.println("\t merhaba \ndunya");
+
+        // birinci ders buraya kadardı
+
+        // ders iki
+        byte abc =  127 ;
+        System.out.println("bytedegeri : " + abc);
+        short shortdegiskeni = 1000;
+        System.out.println("shortdegiskeni : " + shortdegiskeni);
+        int     intdeger = 100000;
+        System.out.println("intdegeri : " + intdeger );
+        long longdegeri = 1000000000;
+        System.out.println("longdegeri : " + longdegeri);
+        /**2.
+         * ders
+         * bitti
+         */
+        float sayi1 = 34.5F;
+        float sayi2 = 35f;
+        float sayi3 = 36f;
+        System.out.println(sayi1);
+        double ondaliklisayi1 = 3.12345;
+        ondaliklisayi1 = 54.1;
+        System.out.println(ondaliklisayi1);
+
+            char karekter = 117;
+            char karekter2 = 'u';
+        System.out.println(karekter2);
+        System.out.println(karekter);
+         boolean mantik = true;
+         boolean mantik2 = false;
+        System.out.println(mantik);
+        System.out.println(mantik2);
+        //
+        String str = "hello world";   // ilk harf büyük yazılmalı
+        //String metin = hello world; şeklinde yazamayız parantez içine yazılmalı
+        System.out.println(str);
+
+
+
+
+
+
+    }
+}
