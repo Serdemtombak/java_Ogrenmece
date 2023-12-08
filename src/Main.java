36738de1ae363b77ffa5f3c081771def56aee44b
Serdemtombak <serdem.tombak@gmail.com>
2023-12-08 16:49:47 +0300

    temel operatörler.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
@@ -50,54 +51,67 @@ public class Main {
         //System.out.println("15" + 20);
         //System.out.println(15 + 20);
         //operatörler
-        int a =10;
-        int b =5;
-        int toplam = a+b;
-        int cikarma = a-b;
-        int carpma =a*b;
-        int bölme = a/b;
-        int mod = a%b;
-        System.out.println("toplam :" + toplam);
-        System.out.println("çıkarma :" + cikarma);
-        System.out.println("çarpma :" + carpma);
-        System.out.println("bölme :" + bölme);
-        System.out.println("mod :" + mod);
-        toplam++;
-        System.out.println(toplam);
-        cikarma--;
-        System.out.println(cikarma);
-        int sonuc = a++ + b--;
-        System.out.println(sonuc);
-        System.out.println(a);
-        System.out.println(b);
+       // int a =10;
+       //  int b =5;
+       // int toplam = a+b;
+       // int cikarma = a-b;
+       // int carpma =a*b;
+       // int bölme = a/b;
+       // int mod = a%b;
+       // System.out.println("toplam :" + toplam);
+       // System.out.println("çıkarma :" + cikarma);
+       // System.out.println("çarpma :" + carpma);
+       // System.out.println("bölme :" + bölme);
+       // System.out.println("mod :" + mod);
+       // toplam++;
+       // System.out.println(toplam);
+       // cikarma--;
+       // System.out.println(cikarma);
+       // int sonuc = a++ + b--;
+       // System.out.println(sonuc);
+       // System.out.println(a);
+       // System.out.println(b);
 
-        int sayi1 = 10;
-        int sayi2 = 5;
-        boolean koşul = (sayi1==sayi2);
-        System.out.println(koşul);
-        boolean koşul1 = (sayi1>sayi2);
-        System.out.println(koşul1);
-        boolean koşul2 = (sayi1/sayi2) == 0;
-        System.out.println(koşul2);
-        boolean koşul3 = (sayi1>sayi2);
-        boolean koşul4 = (sayi2<sayi1);
-        boolean sonuç2 = koşul3&&koşul4;
-        System.out.println(sonuç2);
-        sonuç2 = koşul3||koşul4;
-        System.out.println(sonuç2);
-        String sonuç3 = (koşul3) ? "dogru" : "yanlış";
-        System.out.println(sonuç3);
-        sayi1 = sayi2;
-        System.out.println(sayi1);
-        sayi1 +=2;
-        System.out.println(sayi1);
+       // int sayi1 = 10;
+       // int sayi2 = 5;
+       // boolean koşul = (sayi1==sayi2);
+       // System.out.println(koşul);
+       // boolean koşul1 = (sayi1>sayi2);
+       // System.out.println(koşul1);
+       // boolean koşul2 = (sayi1/sayi2) == 0;
+       // System.out.println(koşul2);
+       // boolean koşul3 = (sayi1>sayi2);
+       // boolean koşul4 = (sayi2<sayi1);
+       // boolean sonuç2 = koşul3&&koşul4;
+       // System.out.println(sonuç2);
+       // sonuç2 = koşul3||koşul4;
+       // System.out.println(sonuç2);
+       // String sonuç3 = (koşul3) ? "dogru" : "yanlış";
+       // System.out.println(sonuç3);
+       // sayi1 = sayi2;
+       // System.out.println(sayi1);
+       // sayi1 +=2;
+       // System.out.println(sayi1);
+        Scanner input = new Scanner(System.in);
+        int a,b;
 
+       /* System.out.println("a sayısını giriniz:");
+        a= input.nextInt();
+        System.out.println("b sayısını giriniz:");
+        b= input.nextInt();
+        System.out.println("a sayısı :" + a);
+        System.out.println("b sayısı :" + b);*/
 
+        /* double c;
+        System.out.println("c sayısını giriniz"); // sayı 12,2 şeklinde yazılır virgül konur nokta değil!
+        c= input.nextDouble();
+        System.out.println("c sayısı :" + c);*/
 
-
-
-
-
+        String str;
+        //str = input.nextLine();
+        //System.out.println(str);
+        str=input.next();
+        System.out.println(str);
 
 
 
